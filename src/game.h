0f3605869942dc1795649aff0948a9068ef39dff
frostmorda <frost.nsk@.gmail.com>
2023-12-03 21:18:43 +0700

    add collision function

--- src/game.h
+++ src/game.h
@@ -16,6 +16,7 @@ public:
     void ProcessInpud() override;
     void Update(float delta_time) override{};
     void Render() override;
+    void Collision() override;
 
 private:
     std::shared_ptr<Player> player_;
